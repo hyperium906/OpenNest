c3826760107a30434c6dcfeedabc26e3e280ef2f
hyperium906 <joshuadupati@gmail.com>
2026-03-29 18:01:43 -0400

    Initial Commit

---
+++ OpenNest/OpenNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenNestApp.swift
+//  OpenNest
+//
+//  Created by Joshua Dupati on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
